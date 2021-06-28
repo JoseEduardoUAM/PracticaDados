160618ee7094a7c3ce04846804f818de258a1342
JoseEduardoUAM <al2172002687@azc.uam.mx>
2021-06-28 15:30:17 -0500

    Clase entidad que permite almacenar el valor de una cara de un dado

---
+++ src/practicadados/Cara.java
@@ -0,0 +1,17 @@
+package practicadados;
+
+public class Cara {
+    
+    private int Valor;
+    
+    public Cara(){
+    }
+    
+    public void setValor(int Valor){
+       this.Valor = Valor; 
+    }   
+    public int getValor(){
+        return Valor;
+    }
+    
+}
