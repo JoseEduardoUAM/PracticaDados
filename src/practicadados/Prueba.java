ee441c9922a178d3f728407f2334911b5391691a
JoseEduardoUAM <al2172002687@azc.uam.mx>
2021-06-28 15:23:54 -0500

    Programa principal

---
+++ src/practicadados/Prueba.java
@@ -0,0 +1,13 @@
+package practicadados;
+
+public class Prueba {
+    public static void main( String [] args ){
+
+        Gestor g = new Gestor();
+        g.agregarDados(6);
+        g.lanzarDados();
+        g.mostrarFrecuencia();
+        g.mostrarHistograma();
+
+    }
+}
